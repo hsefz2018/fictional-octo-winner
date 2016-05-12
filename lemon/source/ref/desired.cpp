7aaddc002b725c0911526691c08cfbfc6fccbc39
Pisces000221 <1786762946@qq.com>
2016-05-12 14:48:54 +0800

    加入 lemon 评测和数据

--- lemon/source/ref/desired.cpp
+++ lemon/source/ref/desired.cpp
@@ -1,76 +1,80 @@
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
-static const int MAXN = 1024;
-static const int MAXM = MAXN * MAXN / 2;
-typedef double mat[MAXN][MAXN];
-int mat_size = 1;
-mat MAT_IDENTITY = {{ 1.0 }};
+static const int MAXN = 512;
+template <typename T> inline T abs(T x) { return x < 0 ? -x : x; }
+
+template <typename T> double *gaussian_elimination(int n, T a[MAXN][MAXN])
+{
+    for (int i = 0; i < n - 1; ++i) {
+        // Partial pivoting
+        double maxval = abs(a[i][i]);
+        int maxidx = i;
+        for (int j = i + 1; j < n; ++j) if (abs(a[j][i]) > maxval) {
+            maxval = abs(a[j][i]);
+            maxidx = j;
+        }
+        if (maxidx != i) {
+            // Swap lines #i and #maxidx
+            for (int j = 0; j <= n; ++j)
+                std::swap(a[i][j], a[maxidx][j]);
+        }
+        // Elimination
+        for (int j = i + 1; j < n; ++j) {
+            for (int k = i + 1; k <= n; ++k)
+                a[j][k] -= a[i][k] * a[j][i] / a[i][i];
+            a[j][i] = 0;
+        }
+    }
+
+    double *x = new double[MAXN]();
+    for (int i = n - 1; i >= 0; --i) {
+        x[i] = a[i][n] / a[i][i];
+        for (int j = i - 1; j >= 0; --j)
+            a[j][n] -= a[j][i] * x[i];
+    }
+    return x;
+}
 
 int n, m;
 bool a[MAXN][MAXN];
-int out[MAXN] = { 0 };
-double edge_w[MAXM];
-mat g;
-
-inline void mat_init(int sz)
-{
-	mat_size = sz;
-	for (int i = 0; i < sz; ++i)
-		MAT_IDENTITY[i][i] = 1.0;
-}
-inline void mat_assign(mat a, mat b)
-{
-	for (register int i = 0; i < mat_size; ++i)
-		memcpy(a[i], b[i], mat_size * sizeof(a[0][0]));
-}
-inline void mat_mul(mat a, mat b)
-{
-	static mat ans;
-	for (int i = 0; i < mat_size; ++i) {
-		for (int j = 0; j < mat_size / 2; ++j) {
-			ans[i][j] = 0.0;
-			for (register int k = 0; k < mat_size; ++k)
-				ans[i][j] += a[i][k] * b[k][j];
-		}
-		for (register int j = mat_size / 2; j < mat_size; ++j)
-			ans[i][j] = a[i][j];
-	}
-	mat_assign(a, ans);
-}
+int deg[MAXN];
+double g[MAXN][MAXN] = {{ 0 }};
 
 int main()
 {
-	freopen("desired.in", "r", stdin);
-	freopen("desired.out", "w", stdout);
+    freopen("desired.in", "r", stdin);
+    freopen("desired.out", "w", stdout);
 
-	scanf("%d%d", &n, &m);
-	mat_init(n + n);
-	int u, v;
-	for (int i = 0; i < m; ++i) {
-		scanf("%d%d", &u, &v); --u, --v;
-		a[u][v] = a[v][u] = true;
-		++out[u]; ++out[v];
-	}
-	for (int i = 0; i < n - 1; ++i)
-		for (int j = 0; j < n; ++j)
-			g[i][j] = a[i][j] ? (1.0 / out[i]) : 0.0;
-	for (int i = 0; i < n; ++i)
-		g[i + n][i] = g[i + n][i + n] = 1.0;
-	for (int i = 0; i < 16; ++i) mat_mul(g, g);
+    scanf("%d%d", &n, &m);
+    int u, v;
+    do {
+        scanf("%d%d", &u, &v); --u, --v;
+        a[u][v] = a[v][u] = true;
+        ++deg[u]; ++deg[v];
+    } while (--m);
 
-	int edge_ct = 0;
-	for (int i = 0; i < n - 1; ++i)
-		for (int j = i + 1; j < n; ++j) if (a[i][j]) {
-			edge_w[edge_ct++] = g[n][i] / out[i] + (j == n - 1 ? 0 : g[n][j] / out[j]);
-		}
-	std::sort(edge_w, edge_w + m);
-	double ans = 0.0;
-	for (int i = 0; i < m; ++i)
-		ans += (m - i) * edge_w[i];
-	printf("%.3lf\n", ans);
+    for (u = 0; u < n; ++u) {
+        for (v = 0; v < n - 1; ++v)
+            if (a[u][v]) g[u][v] = 1.0 / deg[v];
+    }
+    for (int i = 0; i < n; ++i)
+        g[i][i] -= 1.0;
+    g[0][n] = -1.0;
+    double *c_vert = gaussian_elimination(n, g);
 
-	fclose(stdin); fclose(stdout);
-	return 0;
+    double c_edge[MAXN * MAXN];
+    m = 0;
+    for (u = 0; u < n - 1; ++u)
+        for (v = u + 1; v < n; ++v) if (a[u][v])
+            c_edge[m++] = c_vert[u] / deg[u] + (v == n - 1 ? 0 : c_vert[v] / deg[v]);
+    std::sort(c_edge, c_edge + m);
+
+    double ans = 0;
+    for (int i = 0; i < m; ++i)
+        ans += (m - i) * c_edge[i];
+    printf("%.3lf\n", ans);
+
+    fclose(stdin); fclose(stdout);
+    return 0;
 }
 
